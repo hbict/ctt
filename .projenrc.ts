c3f58c6f930eb9aa32c79ad8d34a90fad84fce6b
Alex Wendte <mostcolm@gmail.com>
2025-05-23 18:18:18 -0500

    chore: renames things

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,11 @@
-import { cdk } from 'projen';
+import { cdk } from "projen";
 const project = new cdk.JsiiProject({
-  author: 'Alex Wendte',
-  authorAddress: 'mostcolm@gmail.com',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.8.0',
-  name: 'att',
+  author: "Alex Wendte",
+  authorAddress: "mostcolm@gmail.com",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.8.0",
+  name: "@calm/tt",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/mostcolm/att.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: "https://github.com/alexwendte/tt.git",
 });
 project.synth();
