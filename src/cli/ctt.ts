eaed7ba76cfb6f5e19b627313e21368de09e3cd6
Alex Wendte <mostcolm@gmail.com>
2025-06-07 21:25:56 -0500

    feat: continues implementing ctt bin

--- src/cli/ctt.ts
+++ src/cli/ctt.ts
@@ -1,12 +1,29 @@
 import { input, select } from '@inquirer/prompts';
+import { execSync } from 'child_process';
+import fs from 'fs';
 
+import { CalmsTypescriptApp } from '../cta';
+import { CalmsTypescriptBase, CalmsTypescriptBaseOptions } from '../ctb';
+import { CalmsTypescriptPackage } from '../ctp';
 import { TemplateUserFriendly } from '../types';
+
+interface Answers {
+  directory: string;
+  packageJsonName: string;
+  repositoryUrl: string;
+  template: TemplateUserFriendly;
+}
+
+interface Author {
+  email: string;
+  name: string;
+}
 
 // first argument passed into the script
 const argvDirectory = process.argv[2];
 
-const promptUser = async () => {
-  const template = await select({
+const promptUser = async (): Promise<Answers> => {
+  const template = await select<TemplateUserFriendly>({
     choices: [
       TemplateUserFriendly.CalmsTypescriptApp,
       TemplateUserFriendly.CalmsTypescriptPackage,
@@ -45,10 +62,45 @@ const promptUser = async () => {
   };
 };
 
+const generateProject = (answers: Answers, author: Author) => {
+  const options: CalmsTypescriptBaseOptions = {
+    authorEmail: author.email,
+    authorName: author.name,
+    packageJsonName: answers.packageJsonName,
+    repository: answers.repositoryUrl,
+  };
+  if (answers.template === TemplateUserFriendly.CalmsTypescriptApp) {
+    return new CalmsTypescriptApp(options);
+  }
+  if (answers.template === TemplateUserFriendly.CalmsTypescriptBase) {
+    return new CalmsTypescriptBase(options);
+  }
+  return new CalmsTypescriptPackage(options);
+};
+
 const go = async () => {
   const answers = await promptUser();
 
-  return answers;
+  const name = execSync('git config --get user.name').toString().trim();
+  const email = execSync('git config --get user.email').toString().trim();
+
+  const project = generateProject(answers, { email, name });
+  const projectClass = project.constructor.name;
+
+  // ! this needs to write to the directory indicated by answers.directory
+  fs.writeFileSync(
+    '.projenrc.ts',
+    `import { ${projectClass} } from './src/ctp';
+const project = new ${projectClass}({
+  authorEmail: '${email}',
+  authorName: '${name}',
+  packageJsonName: '${answers.packageJsonName}',
+  repository: '${answers.repositoryUrl}',
+});
+project.synth();
+`,
+  );
+  project.synth();
 };
 
 go().catch((err: unknown) => {
